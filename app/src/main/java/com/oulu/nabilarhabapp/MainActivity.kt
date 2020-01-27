0c552e7302b7d09cb46c230a8c64c44766189c8e
nabil <arhabnabil7@gmail.com>
2020-01-27 11:10:03 +0200

    Add Time and Map activitties. Add 3 Buttons to Main activity with their clicklisners

--- app/src/main/java/com/oulu/nabilarhabapp/MainActivity.kt
+++ app/src/main/java/com/oulu/nabilarhabapp/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.oulu.nabilarhabapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
+        val button3: Button = findViewById(R.id.button3)
+        button1.setOnClickListener {
+            val intent = Intent(this@MainActivity, MapActivity::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener{
+            val intent = Intent(this@MainActivity, TimeActivity::class.java)
+            startActivity(intent)
+        }
+        button3.setOnClickListener {
+            Toast.makeText(this, "Button 3 clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
